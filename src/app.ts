faf452f904b11d5bd41323b95f9b9454e5c3c950
rohithmsr <rohithmsr2001@gmail.com>
2021-03-13 14:01:35 +0530

    fix #3 : Create Node+TS application

---
+++ src/app.ts
@@ -0,0 +1,10 @@
+import express, { Application, Request, Response, NextFunction } from 'express';
+
+const app: Application = express();
+
+app.get('/', (req: Request, res: Response, next: NextFunction) => {
+    res.send('Hello Open Source Champs from Opportunity Calendar Backend !!');
+});
+
+
+app.listen(5000, () => console.log('Server Running at port 5000'));
