3446b09f84ae61cd2d6b59e942923fbada42b623
Abdus <abdus@abdus.net>
2021-03-13 16:26:35 +0530

    format code

--- src/app.ts
+++ src/app.ts
@@ -3,8 +3,7 @@ import express, { Application, Request, Response, NextFunction } from 'express';
 const app: Application = express();
 
 app.get('/', (req: Request, res: Response, next: NextFunction) => {
-    res.send('Hello Open Source Champs from Opportunity Calendar Backend !!');
+  res.send('Hello Open Source Champs from Opportunity Calendar Backend !!');
 });
-
 
 app.listen(5000, () => console.log('Server Running at port 5000'));
