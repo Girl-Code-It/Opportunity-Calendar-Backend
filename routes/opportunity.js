b8aa41c28d5cc0b8333196d152df6cfa9e6e56ef
HaneeshaVakkalagadda <haneesha.v18@iiits.in>
2021-03-24 14:59:08 +0530

    Documented POST API

--- routes/opportunity.js
+++ routes/opportunity.js
@@ -12,19 +12,59 @@ const opportunityManager = new OpportunityManager(),
  * @openapi
  * /opportunity:
  *  post:
- *    summary: create a new opportunity 
- *    description: creates a new opportunity in database
- *    parameters:
- *      - in: body
- *        name: name
- *        description: name of the opportunity
- *        schema:
- *          type: string
- *        required: true
+ *    summary: Creates a new opportunity 
+ *    description: Creates a new opportunity in database
+ *    requestBody:
+ *      required: true
+ *      content:
+ *        application/json:
+ *          schema:
+ *            type: object
+ *            required:
+ *              - opportunityType
+ *              - opportunityTitle
+ *              - opportunityOrganisation
+ *              - opportunityDescription
+ *              - opportunityURL
+ *            properties:
+ *              opportunityId:
+ *                type: string
+ *                description: The ID of the opportunity
+ *              opportunityTitle:
+ *                type: string
+ *                description: Title of the opportunity
+ *              opportunityType:
+ *                type: string
+ *                description: Type of the opportunity
+ *              opportunityOrganisation:
+ *                type: string
+ *                description: Name of organisation providing the opportunity
+ *              opportunityLocation:
+ *                type: string
+ *                description: Location of the opportunity
+ *              opportunityDescription:
+ *                type: string
+ *                description: Description of the opportunity
+ *              opportunityEligibility:
+ *                type: string
+ *                description: Eligibility for the opportunity
+ *              opportunityRegistrationDeadline:
+ *                type: string
+ *                format: date
+ *                description: Registration Deadline for the opportunity
+ *              opportunityDate:
+ *                type: string
+ *                format: date
+ *                description: Date of the opportunity
+ *              opportunityURL:
+ *                type: string
+ *                description: URL of the opportunity
+ * 
  *    responses:
- *      200:
- *        description: document the API response on success here
- *            
+ *      201:
+ *        description: Opportunity is successfully created!
+ *      400:
+ *        description: Sorry, opportunity could not be created!
  */
 router.post('/', (req, res) => {
   console.log('Inside Route');
