40cbdf93907023002e66a7b0d3a9ee8df1cbf013
rohithmsr <64753877+rohithmsr@users.noreply.github.com>
2021-03-24 20:59:42 +0530

    Feature/get opportunities (#21)
    * Add GET Oppotunities With Filter Based on Opportunity Type
    
    * Update API Documentation for GET Opportunities
    
    * Add Swagger Docs for GET Opportunities

--- index.js
+++ index.js
@@ -36,7 +36,10 @@ mongoose
 app.use(cors());
 // in order to read HTTP POST data , we have to use "body-parser" node module. body-parser is a piece of express middleware that reads a form's input and stores it as a javascript object accessible through req.body
 // app.use(bodyParser.urlencoded({ extended: true })); //middleware for parsing bodies from URL.
-app.use(bodyParser.json());
+//app.use(bodyParser.json());
+// express has got its own middleware for bodyparsing, use this as an alternative to bodyparser.json()
+app.use(express.json());
+
 app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/public'));
 app.use(methodOverride('_method')); //to support HTTP Verbs other than GET,POST
