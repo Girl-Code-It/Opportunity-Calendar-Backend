a2b9a19b0bdb91f4d50aafd1e964e8e690b4d149
Samriddhi <Agrawalsamriddhi83@gmail.com>
2021-05-08 23:22:42 +0530

    Update authenticate.js

--- routes/authenticate.js
+++ routes/authenticate.js
@@ -66,20 +66,20 @@ router.get(
 
 router.get(
     '/google-callback',
-    passport.authenticate('google',{failureRedirect: '/auth/login' }),
+    passport.authenticate('google',{failureRedirect : '/auth/login'}),
     (req,res)=>{
         (async()=>{
             const result = await authController.upsertUser(req.user);
             return result;
         })()
         .then(data => {
-             const url = FRONT_END.url + FRONT_END.login_route + "/" + data;
-             res.redirect(url);
-         })
-         .catch(err => {
-             console.log(err);
-             res.redirect(FRONT_END.url + FRONT_END.error_handler + "?err=" + JSON.stringify(err));
-         })
+            const url = FRONT_END.url + FRONT_END.login_route + "/" + data;
+            res.redirect(url);
+        })
+        .catch(err=>{
+            console.log(err);
+            res.redirect(FRONT_END.url + FRONT_END.error_handler + "?err=" + JSON.stringify(err));
+        })
     })
 
 
